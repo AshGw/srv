370895c36d2de6d7218a37b345cb452077e3043b
AshrefGw <AshGw@users.noreply.github.com>
2023-10-17 20:06:40 +0000

    Editor configs set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
-import "./styles/globals.scss";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import './styles/globals.scss';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import SessionProvider from "@/app/components/extras/SessionProvider";
-import { getServerSession } from "next-auth";
+import SessionProvider from '@/app/components/extras/SessionProvider';
+import { getServerSession } from 'next-auth';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Rovxr",
-  description: "Your personalized AI art made with one word",
+  title: 'Rovxr',
+  description: 'Your personalized AI art made with one word',
 };
 
 export default async function RootLayout({
