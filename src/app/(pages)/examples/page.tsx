b831f3ddf61ad7052e52bddae4ee3f4768415fa3
AshrefGw <AshGw@users.noreply.github.com>
2023-10-20 15:22:45 +0000

    Exmaples card set

---
+++ src/app/(pages)/examples/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ExampleCard from '@/app/components/pages/examples/example-card'
+
+export default function page() {
+  return (
+    <div>
+        <ExampleCard></ExampleCard>
+    </div>
+  )
+}
