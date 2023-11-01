24ff717edd4bb299f50a3a90f60fd3d60b141296
AshrefGw <AshGw@users.noreply.github.com>
2023-11-01 01:56:15 +0000

    Issues resolved for TS compiler

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,5 @@
-'server-only';
+import NextAuth from 'next-auth';
+import { authOptions } from './authConfig';
 
-import { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import NextAuth from 'next-auth/next';
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET as string,
-  theme: {
-    colorScheme: 'auto',
-    logo: process.env.LOGO_URL as string,
-    brandColor: '#ffffff',
-    buttonText: '#000000',
-  },
-  providers: [
-    GoogleProvider({
-      clientId: isDevelopment
-        ? (process.env.DEV_GOOGLE_CLIENT_ID as string)
-        : (process.env.GOOGLE_CLIENT_ID as string),
-      clientSecret: isDevelopment
-        ? (process.env.DEV_GOOGLE_CLIENT_SECRET as string)
-        : (process.env.GOOGLE_CLIENT_SECRET as string),
-    }),
-  ],
-  debug: process.env.NEXT_ENV === 'development',
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
