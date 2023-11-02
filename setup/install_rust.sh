04df5ec727fbc62aab6068393a1f0ae39fee7059
AshrefGw <AshGw@users.noreply.github.com>
2023-11-02 21:02:13 +0000

    Container configs

--- setup/install_rust.sh
+++ setup/install_rust.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 if command -v rustc &>/dev/null && command -v cargo &>/dev/null; then
     echo "Rust is already installed."
 else
     apt-get update
     apt install curl -y
     curl https://sh.rustup.rs -sSf | sh -s -- -y
-    source "$HOME/.cargo/env"
+
+    export PATH="$HOME/.cargo/bin:$PATH"
+    rustup default stable
+
     rustc --version
     cargo --version
 fi
